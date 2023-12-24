0711f322a1f1bc0d58e805413ee1b5e91737ffe7
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-12-25 00:25:24 +0530

    Initial Commit

---
+++ StayActiv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StayActiv
+//
+//  Created by Divyansh Bhardwaj on 25/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StayActiv/StayActivApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayActivApp.swift
+//  StayActiv
+//
+//  Created by Divyansh Bhardwaj on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StayActivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
